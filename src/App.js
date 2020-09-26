788830c17907cdf65b41ddc497bc0dc248420cab
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2020-09-26 06:22:59 -0400

    CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content-wrapper">
+        <header><span role="img" aria-label="wave">💝</span> From <a href="https://twitter.com/Hi_Im_Van" target="_blank" rel="noopener noreferrer">Van S.</a></header>
+        <div className="title">VAN SIRISOUK</div>
+        <div className="subtitle">Woah! Hey there stranger <span role="img" aria-label="wave">👋</span></div>
+        <div className="button-list">
+          <a href="temporary"><button>About Me</button></a>
+          <a href="temporary"><button>Experience</button></a>
+          <a href="temporary"><button>Skills</button></a>
+          <a href="temporary"><button>Projects</button></a>
+        </div>
+
+        
+      </div>
     </div>
   );
 }
