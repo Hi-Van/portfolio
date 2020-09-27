3955ef97f7b8882b8ae14880fb98cfc9840f64a1
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2020-09-27 04:18:53 -0400

    Add about section and resume

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import './App.css';
+import { Link } from 'react-scroll';
+import { Resume } from './Resume_Vanouphon_Sirisouk.pdf';
 
 function App() {
   return (
     <div className="App">
       <div className="content-wrapper">
-        <header><span role="img" aria-label="wave">💝</span> From <a href="https://twitter.com/Hi_Im_Van" target="_blank" rel="noopener noreferrer">Van S.</a></header>
         <div className="title">VAN SIRISOUK</div>
-        <div className="subtitle">Woah! Hey there stranger <span role="img" aria-label="wave">👋</span></div>
+        <div className="subtitle">Woah! Hey there stranger <span role="img" className="wave" aria-label="wave">👋</span></div>
         <div className="button-list">
-          <a href="temporary"><button>About Me</button></a>
-          <a href="temporary"><button>Experience</button></a>
-          <a href="temporary"><button>Skills</button></a>
-          <a href="temporary"><button>Projects</button></a>
+          <Link to="about-me" smooth={true} duration={600}><button>About Me</button></Link>
+          <a href={Resume} target="_blank" rel="noopener noreferrer"><button>Resume</button></a>
+          <Link to="about-me" smooth={true} duration={600}><button>Experience</button></Link>
+          <Link to="about-me" smooth={true} duration={600}><button>Skills</button></Link>
+          <Link to="about-me" smooth={true} duration={600}><button>Projects</button></Link>
         </div>
 
-        
+        <div classname="about-section" id="about-me">
+          <div className="about-title">ABOUT ME</div>
+          <div className="about-text">
+            Hey, I'm Van.
+          I'm currently a sophomore at the <a className="reference" href="https://uconn.edu/" target="_blank" rel="noopener noreferrer">University of Connecticut. </a>
+          I'm the founder of the UConn Stamford Engineer Majors, a group of collaborative students at the UConn Stamford campus.
+          I've recently been a part of an initiative group called <a className="reference" href="https://summerofshipping.com/" target="_blank" rel="noopener noreferrer">Summer of Shipping </a> as a core team member and frontend developer.
+          You can find me scrolling through Twitter, making commits on projects on GitHub, or increasing the size of my network on LinkedIn.
+          </div>
+        </div>
       </div>
     </div>
   );
