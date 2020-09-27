441ddf75844dfc5ecea40d13b1cf25ec555babcb
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2020-09-27 07:17:09 -0400

    add skills & experience

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import './App.css';
 import { Link } from 'react-scroll';
+import About from './sections/about-me';
+import Exp from './sections/experience';
+import Skills from './sections/skills';
 
 function App() {
   return (
     <div className="App">
       <div className="content-wrapper">
-        <div className="title">VAN SIRISOUK</div>
-        <div className="subtitle">Woah! Hey there stranger <span role="img" className="wave" aria-label="wave">👋</span></div>
-        <div className="button-list">
-          <Link to="about-me" smooth={true} duration={600}><button>About Me</button></Link>
-          <a href="https://drive.google.com/file/d/1CGvn6LIRuMlptoxdWCQZpyj8pj1KiD-N/view?usp=sharing" target="_blank" rel="noopener noreferrer"><button>Resume</button></a>
-          <Link to="about-me" smooth={true} duration={600}><button>Experience</button></Link>
-          <Link to="about-me" smooth={true} duration={600}><button>Skills</button></Link>
-          <Link to="about-me" smooth={true} duration={600}><button>Projects</button></Link>
-        </div>
-
-        <div classname="about-section" id="about-me">
-          <div className="about-title">ABOUT ME</div>
-          <div className="about-text">
-            Hey, I'm Van.
-          I'm currently a sophomore at the <a className="reference" href="https://uconn.edu/" target="_blank" rel="noopener noreferrer">University of Connecticut. </a>
-          I'm the founder of the UConn Stamford Engineer Majors, a group of collaborative students at the UConn Stamford campus.
-          I've recently been a part of an initiative group called <a className="reference" href="https://summerofshipping.com/" target="_blank" rel="noopener noreferrer">Summer of Shipping </a> as a core team member and frontend developer.
-          You can find me scrolling through Twitter, making commits on projects on GitHub, or increasing the size of my network on LinkedIn.
+        <div id="home">
+          <div className="title">VAN SIRISOUK</div>
+          <div className="subtitle">Woah! Hey there stranger <span role="img" className="wave" aria-label="wave">👋</span></div>
+          <div className="button-list">
+            <Link to="about-me" smooth={true} duration={1000}><button>About Me</button></Link>
+            <a href="https://drive.google.com/file/d/1CGvn6LIRuMlptoxdWCQZpyj8pj1KiD-N/view?usp=sharing" target="_blank" rel="noopener noreferrer"><button>Resume</button></a>
+            <Link to="experience" smooth={true} duration={1000}><button>Experience</button></Link>
+            <Link to="skills" smooth={true} duration={1000}><button>Skills</button></Link>
+            <Link to="about-me" smooth={true} duration={1000}><button>Projects</button></Link>
           </div>
         </div>
+        <About />
+        <Exp />
+        <Skills />
       </div>
     </div>
   );
---
+++ src/sections/about-me.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div id="about-me">
+            <p className="about-text">
+            <div className="about-title">ABOUT ME</div>
+            Hey, I'm Van.
+            I'm currently a sophomore at the <a className="reference" href="https://uconn.edu/" target="_blank" rel="noopener noreferrer">University of Connecticut. </a>
+            I'm the founder of the UConn Stamford Engineer Majors, a group of collaborative students at the UConn Stamford campus.
+            I've recently been a part of an initiative group called <a className="reference" href="https://summerofshipping.com/" target="_blank" rel="noopener noreferrer">Summer of Shipping </a> as a core team member and frontend developer.
+            When I'm not drowning in my coursework, I like to work out and watch YouTube tutorials I'll never do.
+            You can also find me scrolling through <a className="reference" href="https://twitter.com/Hi_Im_Van" target="_blank" rel="noopener noreferrer">Twitter</a>,
+            making commits on <a className="reference" href="https://github.com/Hi-Van" target="_blank" rel="noopener noreferrer">GitHub</a>,
+            or networking on <a className="reference" href="https://www.linkedin.com/in/van-s/" target="_blank" rel="noopener noreferrer">LinkedIn</a>.
+          </p>
+        </div>
+    );
+}
+
+export default About;
