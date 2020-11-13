ed7b896fd93de88857df26305355ab8db8cb3fa7
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2020-11-13 03:17:45 -0500

    homepage revamp

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
 import './App.css';
-import { Link } from 'react-scroll';
-import About from './sections/about-me';
-import Exp from './sections/experience';
-import Skills from './sections/skills';
-import Projects from './sections/projects';
+import logo from './perspective.svg';
+import Tilt from 'react-parallax-tilt';
+import { Switch, Route, Link } from 'react-router-dom';
+import { FaGithub, FaTwitter, FaLinkedin, FaFileInvoice } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
-      <div className="content-wrapper">
-        <div id="home">
-          <div className="title">VAN SIRISOUK</div>
-          <div className="subtitle">Woah! Hey there stranger <span role="img" className="wave" aria-label="wave">👋</span></div>
-          <div className="button-list">
-            <Link to="about-me" smooth={true} duration={1000}><button>About Me</button></Link>
-            <a href="https://drive.google.com/file/d/1CGvn6LIRuMlptoxdWCQZpyj8pj1KiD-N/view?usp=sharing" target="_blank" rel="noopener noreferrer"><button>Resume</button></a>
-            <Link to="experience" smooth={true} duration={1000}><button>Experience</button></Link>
-            <Link to="skills" smooth={true} duration={1000}><button>Skills</button></Link>
-            <Link to="projects" smooth={true} duration={1000}><button>Projects</button></Link>
+      <div className='homepage'>
+        <Tilt flipHorizontally={true}>
+          <img className='logo' src={logo} alt='logo' />
+        </Tilt>
+        <div>
+          <div className='grid'>
+            <Link><button className='btn-link'>About Me</button></Link>
+            <Link><button className='btn-link'>Experience</button></Link>
+            <Link><button className='btn-link'>Projects</button></Link>
+          </div>
+          <div className='btn-container'>
+            <a href='https://react-icons.github.io/' target='_blank' rel='noopener noreferrer'><button className='github-btn'><FaGithub /></button></a>
+            <a href='https://react-icons.github.io/' target='_blank' rel='noopener noreferrer'><button className='twitter-btn'><FaTwitter /></button></a>
+            <a href='https://react-icons.github.io/' target='_blank' rel='noopener noreferrer'><button className='linkedin-btn'><FaLinkedin /></button></a>
+            <a href='https://react-icons.github.io/' target='_blank' rel='noopener noreferrer'><button className='resume-btn' title='download resume'><FaFileInvoice /></button></a>
           </div>
         </div>
-        <About />
-        <Exp />
-        <Skills />
-        <Projects />
       </div>
+
     </div>
   );
 }
