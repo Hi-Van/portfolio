4d9d0d17b92f6af603a45d6193d6573a58a48306
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2021-01-07 13:15:09 -0500

    import fuse.js

--- src/project.js
+++ src/project.js
@@ -1,17 +1,66 @@
 import './project.css';
-import prep from './preeepare.PNG';
-import cloudy from './cloudy.JPG';
-import picky from './not-picky.JPG';
-import oldPort from './port-dep.JPG';
+import { useState } from 'react';
 import newPort from './new-port.PNG';
-import run from './runtime.JPG';
-import wiki from './sos-wiki.JPG';
 import { FaLink, FaBookOpen } from "react-icons/fa";
 import Fade from 'react-reveal/Fade';
+import Fuse from 'fuse.js';
+import links from './projects.json';
 
 function Projects() {
+
+    const [query, setQuery] = useState('');
+    const [test, setTest] = useState('');
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        setQuery(test);
+    }
+
+    const fuse = new Fuse(links, {
+        keys: [
+            'title',
+            'tech',
+            'desc'
+        ],
+        threshold: '0.35'
+    })
+
+    const results = fuse.search(query);
+
+    const linkResult = query ? results.map(result => result.item) : links;
+
     return (
         <div className='projects'>
+
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Search Wiki.." value={test} onChange={e => setTest(e.target.value)} className='search' />
+                <input type="submit" value="Search" />
+            </form>
+
+            <div className="grid-search">
+                <button className="search-btn am4">am4Charts</button>
+                <button className="search-btn HTML">HTML</button>
+                <button className="search-btn CSS">CSS</button>
+                <button className="search-btn JS">JavaScript</button>
+                <button className="search-btn Mat">Material UI</button>
+                <button className="search-btn Figma">Figma</button>
+                <button className="search-btn Fuse">FuseJS</button>
+                <button className="search-btn React">ReactJS</button>
+                <button className="search-btn Heroku">Heroku</button>
+                <button className="search-btn Next">NextJS</button>
+                <button className="search-btn Chart">ChartJS</button>
+                <button className="search-btn Google">Google</button>
+            </div>
+
+            {linkResult.map(card => {
+                const { title, link } = card;
+                return (
+                    <a key={title} href={link} target='_blank' rel='noopener noreferrer'>
+                        <button className='btn-info'>{title}</button>
+                    </a>
+                )
+            })}
+
             <Fade bottom>
                 <div className='proj'>
                     <img className='proj-img' src={newPort} alt='prep' />
@@ -22,102 +71,7 @@ function Projects() {
               </p>
                         <div>
                             <a href='https://github.com/Hi-Van/hi-van.github.io' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit documentation'><FaBookOpen /></button></a>
-                        </div>
-                    </div>
-                </div>
-            </Fade>
-
-            <Fade bottom>
-                <div className='proj'>
-                    <img className='proj-img' src={prep} alt='prep' />
-                    <div>
-                        <h1>Preeepare</h1>
-                        <p>Preeepare is an interview prep visualization tool and developer resource center.
-                        Built utilizing ReactJS, ChartJS, and Google Docs
-              </p>
-                        <div>
-                            <a href='https://github.com/Hi-Van/preeepare' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit documentation'><FaBookOpen /></button></a>
-                            <a href='https://hi-van.github.io/preeepare/' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit project'><FaLink /></button></a>
-                        </div>
-                    </div>
-                </div>
-            </Fade>
-
-            <Fade bottom>
-                <div className='proj'>
-                    <img className='proj-img' src={cloudy} alt='prep' />
-                    <div>
-                        <h1>Cloudy</h1>
-                        <p>A word cloud generator web application, hosted on Heroku.
-                        Built using ReactJS, Material UI, and am4Charts Library
-              </p>
-                        <div>
-                            <a href='https://github.com/Hi-Van/Word-Cloud/tree/master/word-cloud' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit documentation'><FaBookOpen /></button></a>
-                            <a href='https://cloudy-word-cloud.herokuapp.com/' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit project'><FaLink /></button></a>
-                        </div>
-                    </div>
-                </div>
-            </Fade>
-
-            <Fade bottom>
-                <div className='proj'>
-                    <img className='proj-img' src={picky} alt='prep' />
-                    <div>
-                        <h1>!Picky // IN PROGRESS</h1>
-                        <p>A restaurant picker based within a react application.
-                        Built using ReactJS, Material UI, Google Maps API, Google Places API, and Google GeoCode API
-              </p>
-                        <div>
-                            <a href='https://github.com/Hi-Van/Not-Picky/tree/master/my-app' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit documentation'><FaBookOpen /></button></a>
-                            <a href='https://not-picky.herokuapp.com/' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit project'><FaLink /></button></a>
-                        </div>
-                    </div>
-                </div>
-            </Fade>
-
-            <Fade bottom>
-                <div className='proj'>
-                    <img className='proj-img' src={wiki} alt='prep' />
-                    <div>
-                        <h1>Summer of Shipping Landing Site</h1>
-                        <p>A website that hosts projects and information about summer of shipping.
-                        Built using NextJS, FuseJS, JSON, and Figma
-                        </p>
-                        <div>
-                            <a href='https://github.com/Hi-Van/sos-landing' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit documentation'><FaBookOpen /></button></a>
-                            <a href='https://summerofshipping.com/' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit project'><FaLink /></button></a>
-                        </div>
-                    </div>
-                </div>
-            </Fade>
-
-            <Fade bottom>
-                <div className='proj'>
-                    <img className='proj-img' src={run} alt='prep' />
-                    <div>
-                        <h1>Runtime</h1>
-                        <p>A website that logs JavaScript and Python code for data structures and algorithms, along with worst case runtimes.
-                        Built using ReactJS and Material UI
-              </p>
-                        <div>
-                            <a href='https://github.com/Hi-Van/runtime-sheet' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit documentation'><FaBookOpen /></button></a>
-                            <a href='https://hi-van.github.io/runtime-sheet/' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit project'><FaLink /></button></a>
-                        </div>
-                    </div>
-                </div>
-            </Fade>
-
-            <Fade bottom>
-                <div className='proj'>
-                    <img className='proj-img' src={oldPort} alt='prep' />
-                    <div>
-                        <h1>Portfolio Website v3</h1>
-                        <p>My old portfolio website, used to host my projects and information about myself.
-                        Built using HTML, CSS, and JavaScript
-              </p>
-                        <div>
-                            <a href='https://github.com/Hi-Van/hi-van.github.io' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit documentation'><FaBookOpen /></button></a>
-                            <a href='https://hi-van.github.io/' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit project'><FaLink /></button></a>
+                            <a href='https://github.com/Hi-Van/hi-van.github.io' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit documentation'><FaLink /></button></a>
                         </div>
                     </div>
                 </div>
