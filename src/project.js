d155ee2767cc2b202583869bb86a04d772722a9e
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2021-01-07 17:51:01 -0500

    FuseJS integration

--- src/project.js
+++ src/project.js
@@ -1,6 +1,5 @@
 import './project.css';
 import { useState } from 'react';
-import newPort from './new-port.PNG';
 import { FaLink, FaBookOpen } from "react-icons/fa";
 import Fade from 'react-reveal/Fade';
 import Fuse from 'fuse.js';
@@ -22,7 +21,6 @@ function Projects() {
             'tech',
             'desc'
         ],
-        threshold: '0.35'
     })
 
     const results = fuse.search(query);
@@ -38,44 +36,56 @@ function Projects() {
             </form>
 
             <div className="grid-search">
-                <button className="search-btn am4">am4Charts</button>
+                <button className="search-btn am4Charts">am4Charts</button>
                 <button className="search-btn HTML">HTML</button>
                 <button className="search-btn CSS">CSS</button>
-                <button className="search-btn JS">JavaScript</button>
-                <button className="search-btn Mat">Material UI</button>
+                <button className="search-btn JavaScript">JavaScript</button>
+                <button className="search-btn MaterialUI">MaterialUI</button>
                 <button className="search-btn Figma">Figma</button>
-                <button className="search-btn Fuse">FuseJS</button>
-                <button className="search-btn React">ReactJS</button>
+                <button className="search-btn FuseJS">FuseJS</button>
+                <button className="search-btn ReactJS">ReactJS</button>
                 <button className="search-btn Heroku">Heroku</button>
-                <button className="search-btn Next">NextJS</button>
-                <button className="search-btn Chart">ChartJS</button>
+                <button className="search-btn NextJS">NextJS</button>
+                <button className="search-btn ChartJS">ChartJS</button>
                 <button className="search-btn Google">Google</button>
             </div>
 
             {linkResult.map(card => {
-                const { title, link } = card;
+                const { title, tech, desc, code, link, img } = card;
+
                 return (
-                    <a key={title} href={link} target='_blank' rel='noopener noreferrer'>
-                        <button className='btn-info'>{title}</button>
-                    </a>
+                    <Fade bottom>
+                        <div className='proj'>
+                            <img className='proj-img' src={img} alt='prep' />
+                            <div>
+                                <h1>{title}</h1>
+                                <div className="grid-search" style={{margin: '12px 0px 0px -16px'}}>
+                                    {tech.map(i => {
+                                        return (
+                                            <button className={i} style={{
+                                                fontFamily: "Raleway",
+                                                letterSpacing: '0.5px',
+                                                fontWeight: '500',
+                                                borderRadius: '4px',
+                                                fontSize: '1.25rem',
+                                                minWidth: '120px',
+                                                margin: '1rem 2rem 0rem 2rem',
+                                                display: 'flex',
+                                                justifyContent: 'center'
+                                            }}>{i}</button>
+                                        )
+                                    })}
+                                </div>
+                                <p>{desc}</p>
+                                <div>
+                                    <a href={code} target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit documentation'><FaBookOpen /></button></a>
+                                    <a href={link} target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit documentation'><FaLink /></button></a>
+                                </div>
+                            </div>
+                        </div>
+                    </Fade>
                 )
             })}
-
-            <Fade bottom>
-                <div className='proj'>
-                    <img className='proj-img' src={newPort} alt='prep' />
-                    <div>
-                        <h1>Portfolio Website v5</h1>
-                        <p>My newest portfolio website, used to host my projects and information about myself.
-                        Built using ReactJS, react-parallax-tilt, react-router-dom, and react-reveal
-              </p>
-                        <div>
-                            <a href='https://github.com/Hi-Van/hi-van.github.io' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit documentation'><FaBookOpen /></button></a>
-                            <a href='https://github.com/Hi-Van/hi-van.github.io' target='_blank' rel='noopener noreferrer'><button className='proj-link' title='visit documentation'><FaLink /></button></a>
-                        </div>
-                    </div>
-                </div>
-            </Fade>
         </div>
     );
 }
