cc919b390a740095a6a9f3646bf7f2a61a9a0ebc
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2021-06-20 21:24:17 -0400

    home buttons

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import hand from '../resources/Rock_On_R-Angle_A4_0000.png';
 import '../stylesheets/home.css';
-import { FcIdea, FcManager, FcOpenedFolder } from 'react-icons/fc';
+import { FcIdea, FcManager, FcOpenedFolder,FcIpad } from 'react-icons/fc';
 import { Link } from 'react-scroll';
 
 const Home = () => {
@@ -12,11 +12,12 @@ const Home = () => {
                 <div>
                     <h1 className='land-title'><span style={{ color: '#685dbb' }}>Van</span>ouphon <br /><span style={{ color: '#685dbb' }}>S</span>irisouk<span style={{ color: '#685dbb' }}>.</span></h1>
                     <h2 className='land-subtitle'>Woah! Hey there stranger<span className='wave'>👋</span></h2>
-                    <Link className='land-link' to='About' smooth={true} duration={500}><FcIdea /> About Me</Link>
-                    <br/>
-                    <Link className='land-link' to='About' smooth={true} duration={500}><FcManager /> Experience</Link>
-                    <br/>
-                    <Link className='land-link' to='About' smooth={true} duration={500}><FcOpenedFolder /> Projects</Link>
+                    <div className='grid'>
+                        <Link className='land-link' to='About' smooth={true} duration={500}><FcIdea/> About Me</Link>
+                        <Link className='land-link' to='About' smooth={true} duration={500}><FcManager/> Experience</Link>
+                        <Link className='land-link' to='About' smooth={true} duration={500}><FcOpenedFolder/> Projects</Link>
+                        <Link className='land-link' to='About' smooth={true} duration={500}><FcIpad/> Resume</Link>
+                    </div>
                 </div>
                 <img src={hand} alt='hand' className='hand floating' />
             </div>
