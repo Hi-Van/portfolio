4571ca9b5d0fea16cfca969f501b45b8f02c84e8
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2022-01-28 19:39:47 -0500

    build homepage and add media assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import logo from './media/landing-logo.svg';
+import { BsFillArrowDownRightCircleFill, BsTwitter, BsGithub } from 'react-icons/bs';
+import { FaLinkedinIn } from 'react-icons/fa';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='content'>
+
+        <div className='page'>
+
+          <img className='logo' src={logo} alt="logo" />
+
+          <div className='flex specialLeft'>
+            <a className="btn yellow pill" href="https://google.com">Learn More<BsFillArrowDownRightCircleFill /></a>
+
+            <div className='flex'>
+              <a className="btn social rounded" href="https://google.com"><FaLinkedinIn /></a>
+              <a className="btn social rounded" href="https://google.com"><BsTwitter /></a>
+              <a className="btn social rounded" href="https://google.com"><BsGithub /></a>
+            </div>
+
+          </div>
+
+        </div>
+
+        <div className='page'>
+          <div className='flex'>
+
+          </div>
+        </div>
+
+
+
+
+      </div>
     </div>
   );
 }
