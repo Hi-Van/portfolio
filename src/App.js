c08b4600775f8d3cb8eb92e0a8550b1d3f826c30
Hi-Van <sirisoukvanouphon@gmail.com>
2022-02-21 19:08:58 -0500

    finish experience section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import exp from './media/WhatExp.png';
 import arrow from './media/arrows.png';
 import { BsFillArrowDownRightCircleFill, BsTwitter, BsGithub } from 'react-icons/bs';
 import { FaLinkedinIn } from 'react-icons/fa';
+import D1 from './media/Aug Date.png';
+import D2 from './media/Feb Date.png';
+import D3 from './media/May Date.png';
+import D4 from './media/May Date 2021.png';
 import './App.css';
 
 function App() {
@@ -53,13 +57,69 @@ function App() {
         </div>
 
         <div className='page'>
-          <img src={exp} alt='exp-title' />
+          <img src={exp} alt='exp-title' style={{ marginBottom: '12rem' }} />
+
+          <div className='job righthand'>
+            <img src={D1} alt='aug date' />
+            <h2>
+              UConn Stamford Engineer Majors
+            </h2>
+            <h3>
+              Founder
+            </h3>
+            <li>Scaled population from 14+ students to 40+ students</li>
+            <li>Directed several teams of students, resulting in an overall reduction in stress & workload</li>
+            <li>Lead a team of 13 collaborative students, decreasing time spent on assignments by 30% - 40% on avg.</li>
+
+          </div>
 
           <img className='arrow' src={arrow} alt='arrow'></img>
+
+          <div className='job'>
+            <img src={D2} alt='aug date' />
+            <h2>
+              University of Connecticut
+            </h2>
+            <h3>
+              CS & Mathematics Tutor
+            </h3>
+            <li>Assisted in analyzing and debugging code with students</li>
+            <li>Communicated technical concepts and issues to students, clarifying confusion with students</li>
+            <li>Created study plans based off of Computer Science & Calculus curriculum for students</li>
+
+          </div>
 
           <img className='arrow flip' src={arrow} alt='arrow'></img>
 
+          <div className='job righthand'>
+            <img src={D3} alt='aug date' />
+            <h2>
+              Summer of Shipping
+            </h2>
+            <h3>
+              Frontend Developer & Team Member
+            </h3>
+            <li>Initiative group for students with cancelled internships for Summer 2020 founded by ex-Uber engineer</li>
+            <li>Scaled population from 20 members to 600+ students & 70+ mentors globally</li>
+            <li>Developed a search feature with FuseJS, enabling users to search internal JSON formatted data</li>
+
+          </div>
+
           <img className='arrow' src={arrow} alt='arrow'></img>
+
+          <div className='job'>
+            <img src={D4} alt='aug date' />
+            <h2>
+              Edgewell Personal Care
+            </h2>
+            <h3>
+              QA & Systems Engineer Intern
+            </h3>
+            <li>Wrote documentation for database maintenance and query modification</li>
+            <li>Developed internal tools for modifying, adding, and deleting database information using HTML, CSS, JavaScript, and Python</li>
+            <li>Proposed and Implemented new repository procedures and development workflow</li>
+
+          </div>
         </div>
 
 
