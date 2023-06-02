5ee68ebc23c25c43070eeadbbe78028a02551348
Hi-Van <sirisoukvanouphon@gmail.com>
2023-06-01 21:30:13 -0400

    start nav menu

---
+++ src/components/navigation.tsx
@@ -0,0 +1,7 @@
+import { Box, Flex, Heading } from "@chakra-ui/react";
+
+export const NavigationMenu = () => {
+    return (<Box h={"fit-content"}>
+        <Flex alignItems={""}><Heading fontWeight={"semibold"} fontFamily='poppins' size="lg" p={1}><span style={{ color: "#6552F5" }}>Van</span>ouphon <span style={{ color: "#6552F5" }}>S</span>irisouk<span style={{ color: "#6552F5" }}>.</span></Heading></Flex>
+    </Box>);
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import App from './App.tsx'
-import './index.css'
+
+const override = extendTheme({
+  fonts: {
+    body: "Inter Tight, sans-serif",
+    heading: "Inter Tight, sans-serif",
+    poppins: "Poppins",
+  },
+
+  styles: {
+    global:
+     {
+      body: {
+        color: "#221E41"
+      }
+     }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={override}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
