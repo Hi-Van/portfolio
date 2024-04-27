cabda10e3bb42cef40cb929cbc08a3b363f405ae
Hi-Van <sirisoukvanouphon@gmail.com>
2024-04-26 22:26:55 -0500

    add about me page and finish landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,71 @@
 import { Separator } from "@/components/ui/separator";
+import { FaLocationArrow } from "react-icons/fa6";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="flex flex-col w-full h-full max-w-[1400px] items-center text-sm">
+      <div className="flex flex-col w-full h-full max-w-[800px] items-center text-sm">
         <h1 className="text-6xl font-semibold mb-2">
           <span className="text-custom-highlight">Van</span>ouphon{" "}
           <span className="text-custom-highlight">S</span>irisouk
           <span className="text-custom-highlight">.</span>
         </h1>
-        <h2 className="text-base dark:text-muted-foreground mb-24">
+        <h2 className="text-base dark:text-muted-foreground">
           Human visualizing{" "}
-          <code className="text-custom-highlight bg-custom-highlight/20 py-0.5 px-1 rounded">
+          <code className="text-custom-highlight bg-custom-highlight/20 py-0.5 px-1 rounded ">
             1&0
           </code>{" "}
           for other humans.
         </h2>
 
-        <div className="flex justify-center gap-6 text-xs text-muted-foreground h-20">
+        <h2 className="inline-flex items-center gap-2 my-8">
+          <FaLocationArrow />
+          <span className="bg-card p-0.5 px-2 rounded border bg-muted/60 text-muted-foreground font-light">
+            Bentonville, AR
+          </span>
+        </h2>
+
+        <h2 className="max-w-96 text-center">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger className="bg-card p-0.5 px-1 rounded border bg-muted/60 text-muted-foreground font-light">
+                Software Engineer
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>I make and break things 😀</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>{" "}
+          working at{" "}
+          <span className="bg-card p-0.5 px-1 rounded border bg-muted/60 text-muted-foreground font-light">
+            Walmart Global Tech
+          </span>
+          , connecting people to products.
+        </h2>
+
+        <div className="flex justify-center gap-6 text-xs text-muted-foreground h-16 mt-12">
           <p className="max-w-52">
             Software Engineer, I focus on improving data accessibility,
             transparency, and consumption for users.
           </p>
-          <Separator orientation="vertical" className="light:bg-muted-foreground"/>
+          <Separator
+            orientation="vertical"
+            className="light:bg-muted-foreground"
+          />
           <p className="max-w-52">
             Web design enthusiast, I enjoy creating beautiful and functional
             spaces.
           </p>
-          <Separator orientation="vertical" className="light:bg-muted-foreground"/>
+          <Separator
+            orientation="vertical"
+            className="light:bg-muted-foreground"
+          />
           <p className="max-w-52">
             Part-time nerd, currently diving into live-collaboration and
             multiplayer technology.
--- components/custom/navigation.tsx
+++ components/custom/navigation.tsx
@@ -4,23 +4,23 @@ import { ThemeToggle } from "./theme-toggle";
 export const Navigation = () => {
   const position = "z-1 fixed w-full top-8 left-1/2 -translate-x-1/2 ";
   const display =
-    "flex justify-between items-center bg-card gap-8 px-4 max-w-[800px] h-10 border rounded-md font-medium text-xs ";
+    "flex justify-between items-center bg-card gap-8 px-4 max-w-[800px] h-10 border rounded-md font-medium text-xs shadow bg-card";
   const inlineFlex = "inline-flex justify-center items-center gap-8";
 
   return (
-    <nav className={position + display + "bg-card"}>
+    <nav className={position + display}>
       <Link href="/">Vanouphon Sirisouk</Link>
       <div className={inlineFlex}>
-        <Link href="/about" className="hover:text-custom-highlight">
+        <Link href="/about" className="primary-foreground hover:text-custom-highlight">
           About
         </Link>
-        <Link href="/experience" className="hover:text-custom-highlight">
+        <Link href="/experience" className="primary-foreground hover:text-custom-highlight">
           Experience
         </Link>
-        <Link href="/projects" className="hover:text-custom-highlight">
+        <Link href="/projects" className="primary-foreground hover:text-custom-highlight">
           Featured Work
         </Link>
-        <ThemeToggle/>
+        <ThemeToggle />
       </div>
     </nav>
   );
