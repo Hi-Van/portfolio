71f559b7e6cab129ff5604c2a3610bb85bd4cf65
Van Sirisouk <van@Vans-MacBook-Pro.local>
2026-03-01 20:06:20 -0500

    checkpoint

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { AuroraText } from '@/components/ui/aurora-text';
+
+export default function Page() {
+	return (
+		<div className='flex flex-col items-center'>
+			<h1 className="text-6xl font-bold mb-2">
+				<AuroraText>Vanouphon Sirisouk.</AuroraText>
+			</h1>
+			<h2 className='text-xl'>
+				Human Visualizing <code>1&0</code> for other humans.
+			</h2>
+		</div>
+	);
+}
